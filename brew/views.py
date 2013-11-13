93c300d56d3d1cd8c8aeafec85963f82a93c954b
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-11-13 23:30:45 +0100

    Add /brew endpoint

--- brew/views.py
+++ brew/views.py
@@ -17,7 +17,12 @@ def recipes():
     return render_template('create.html')
 
 
-@app.route('/prepare/<recipe_id>')
+@app.route('/prepare/<recipe_id>', methods=['GET'])
 def prepare(recipe_id):
     recipe = mongo.db.recipes.find_one(ObjectId(recipe_id))
     return render_template('prepare.html', recipe=recipe)
+
+
+@app.route('/brew/<recipe_id>', methods=['POST'])
+def brew(recipe_id):
+    return "Create new brew with {} L".format(request.form['amount'])
