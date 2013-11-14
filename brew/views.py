711eeeefea451e78b29eb1a7cf814d385fd3b457
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-11-14 08:18:13 +0100

    Unify /brew endpoints

--- brew/views.py
+++ brew/views.py
@@ -17,12 +17,14 @@ def recipes():
     return render_template('create.html')
 
 
-@app.route('/prepare/<recipe_id>', methods=['GET'])
+@app.route('/brews/prepare/<recipe_id>', methods=['GET'])
 def prepare(recipe_id):
     recipe = mongo.db.recipes.find_one(ObjectId(recipe_id))
     return render_template('prepare.html', recipe=recipe)
 
 
-@app.route('/brew/<recipe_id>', methods=['POST'])
-def brew(recipe_id):
-    return "Create new brew with {} L".format(request.form['amount'])
+@app.route('/brews', methods=['POST'])
+def brew():
+    recipe_id = request.form['recipe_id']
+    recipe = mongo.db.recipes.find_one(ObjectId(recipe_id))
+    return "Brew a {} with {} L".format(recipe['name'], request.form['amount'])
