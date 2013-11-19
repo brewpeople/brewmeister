9bf9c825d6e9cf7cf6c1c51fa8d126df565fa575
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2013-11-19 12:14:51 +0100

    Add Makefile for running and translating
    Sometimes, grand daddy make is a better match than these hip script tools.

---
+++ Makefile
@@ -0,0 +1,25 @@
+CONFIG=babel.cfg
+MESSAGES=messages.pot
+TRANSLATIONS_DIR=brew/translations
+
+.PHONY: createpo compilepo init run updatepot
+
+all: run
+
+run: compilepo
+	@python manage.py runserver
+
+init:
+	@python manage.py populatedb
+
+createpo: $(MESSAGES)
+	@echo "\n>>> Enter language code"; read lang_code; pybabel init -i $(MESSAGES) -d $(TRANSLATIONS_DIR) -l $$lang_code
+
+compilepo: $(MESSAGES)
+	@pybabel compile -f -d $(TRANSLATIONS_DIR)
+
+updatepot: $(MESSAGES)
+	@pybabel update -i $(MESSAGES) -d $(TRANSLATIONS_DIR)
+
+$(MESSAGES):
+	@pybabel extract -F $(CONFIG) -o $(MESSAGES) .
--- manage.py
+++ manage.py
@@ -13,26 +13,5 @@ def populatedb():
         mongo.db.recipes.insert(json.load(f))
 
 
-@manager.command
-def updatepot():
-    cmd = 'pybabel extract -F babel.cfg -o messages.pot .'
-    subprocess.check_call(cmd.split())
-
-    cmd = 'pybabel update -i messages.pot -d brew/translations'
-    subprocess.check_call(cmd.split())
-
-
-@manager.command
-def createpo(lang):
-    cmd = 'pybabel init -i messages.pot -d brew/translations -l' + lang
-    subprocess.check_call(cmd.split())
-
-
-@manager.command
-def compilepo():
-    cmd = 'pybabel compile -f -d brew/translations'
-    subprocess.check_call(cmd.split())
-
-
 if __name__ == '__main__':
     manager.run()
